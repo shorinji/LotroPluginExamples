5e0763c2d4270ce2e98460d67802057ea753a97a
Fredrik Fall <fredrik.fall@gmail.com>
2019-08-20 19:37:33 +0200

    Finished example 2 and updated example 1
    - Changed names of both examples, to allow /plugins load <name> to work
    - Removed semicolons from example 1
    - Implemented example 2 to properly display a window with a custom character screen

--- FredriksExamples/Example1/Main.lua
+++ FredriksExamples/Example1/Main.lua
@@ -1,17 +1,17 @@
-import "Turbine"; -- for ShellCommand
-import "Turbine.Gameplay"; -- for GamePlay.LocalPlayer
+import "Turbine" -- for Turbine.Shell and Turbine.ShellCommand
+import "Turbine.Gameplay" -- for Turbine.LocalPlayer
 
 -- Create new chat command
-GreetCommand = Turbine.ShellCommand(); 
+GreetCommand = Turbine.ShellCommand()
 
 -- Declare code to run when the chat command is issued
 function GreetCommand:Execute(command, arguments)
-	-- Fetch a reference to the player's character
-	local player = Turbine.Gameplay.LocalPlayer.GetInstance();
+    -- Fetch a reference to the player's character
+	local player = Turbine.Gameplay.LocalPlayer.GetInstance()
 
-	-- Write text to chat. Double dots concatenate two strings in lua
-	Turbine.Shell.WriteLine("Greetings " .. player:GetName() .. "!");
+    -- Write text to chat. Double dots concatenate two strings in lua, <rgb> is used to color text
+	Turbine.Shell.WriteLine("Greetings <rgb=#FF00FF>" .. player:GetName() .. "!</rgb>")
 end
 
 -- Register the chat command and link the command word to the code
-Turbine.Shell.AddCommand("GreetSelf", GreetCommand);
+Turbine.Shell.AddCommand("GreetSelf", GreetCommand)
--- FredriksExamples/Example2/Main.lua
+++ FredriksExamples/Example2/Main.lua
@@ -3,13 +3,95 @@ import "Turbine.Gameplay"
 import "Turbine.UI"
 import "Turbine.UI.Lotro"
 
+function getRaceName(myRaceId)
+	for raceName, raceId in pairs(Turbine.Gameplay.Race) do
+		if myRaceId == raceId then
+			return raceName
+		end
+	end
+	return "Unknown"
+end
 
-myWindow = Turbine.UI.Lotro.Window()
-myWindow:SetSize(500, 100)
-myWindow:SetPosition(
-	( Turbine.UI.Display.GetWidth() / 2 ) - ( myWindow:GetWidth() / 2 ),
-	( Turbine.UI.Display.GetHeight() / 3 ) - ( myWindow:GetHeight() / 2 )
+function getClassName(myClassId)
+	for className, classId in pairs(Turbine.Gameplay.Class) do
+		if myClassId == classId then
+			return className
+		end
+	end
+	return "Unknown"
+end
+
+
+characterWindow = Turbine.UI.Lotro.Window()
+characterWindow:SetSize(500, 550)
+characterWindow:SetPosition(
+	(Turbine.UI.Display.GetWidth() / 2) - (characterWindow:GetWidth() / 2),
+	(Turbine.UI.Display.GetHeight() / 3) - (characterWindow:GetHeight() / 2)
 )
 
-myWindow:SetVisible(true)
-myWindow:SetText("Jolly good show")
+player = Turbine.Gameplay.LocalPlayer.GetInstance()
+
+nameLabel = Turbine.UI.Label()
+nameLabel:SetParent(characterWindow)
+nameLabel:SetPosition(30, 50)
+nameLabel:SetFont(Turbine.UI.Lotro.Font.TrajanPro14)
+nameLabel:SetText(player:GetName())
+
+descriptionLabel = Turbine.UI.Label()
+descriptionLabel:SetParent(characterWindow)
+descriptionLabel:SetSize(400, 50)
+descriptionLabel:SetPosition(40, 80)
+descriptionLabel:SetMultiline(true)
+
+description = "Level: " .. player:GetLevel() .. "\n"
+   .. "Race: " .. getRaceName(player:GetRace()) .. "\n"
+   .. "Class: " .. getClassName(player:GetClass()) .. "\n"
+   .. "Morale: " .. math.round(player:GetMorale()) .. " / " .. math.round(player:GetMaxMorale()) .. "\n"
+
+descriptionLabel:SetText(description)
+
+equipmentLabel = Turbine.UI.Label()
+equipmentLabel:SetParent(characterWindow)
+equipmentLabel:SetPosition(30, 140)
+equipmentLabel:SetSize(200, 20)
+equipmentLabel:SetFont(Turbine.UI.Lotro.Font.TrajanPro14)
+equipmentLabel:SetText("Equipment")
+
+
+equipmentList = Turbine.UI.ListBox()
+equipmentList:SetParent(characterWindow)
+equipmentList:SetPosition(40, 160)   
+equipmentList:SetSize(400, 350)
+   
+
+equipment = player:GetEquipment()
+for equipmentSlotName, equipmentSlotId in pairs(Turbine.Gameplay.Equipment) do
+	if type(equipmentSlotId) == "number" then
+		item = equipment:GetItem(equipmentSlotId)
+		if item ~= nil then
+			equipmentSlotLabel = Turbine.UI.Label()
+			equipmentSlotLabel:SetPosition(0, 0)
+			equipmentSlotLabel:SetSize(150, 15)
+			equipmentSlotLabel:SetText(equipmentSlotName)
+			
+			slottedItemLabel = Turbine.UI.Label()
+			slottedItemLabel:SetPosition(150, 0)
+			slottedItemLabel:SetSize(400, 15)
+		 	slottedItemLabel:SetText(item:GetName())
+
+			listItem = Turbine.UI.Control()
+			listItem:SetSize(550, 15)
+
+			equipmentSlotLabel:SetParent(listItem)
+			slottedItemLabel:SetParent(listItem)
+
+			equipmentList:AddItem(listItem)
+		end
+	end
+end
+	
+
+
+characterWindow:SetText("Custom character panel")
+characterWindow:SetVisible(true)
+
